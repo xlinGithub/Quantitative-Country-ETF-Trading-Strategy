83453a411a7d0c389070778ee77e1f6b380d3559
xlinGithub <11510523+xlinGithub@users.noreply.github.com>
2020-03-25 14:15:24 -0400

    Update quantopian_backtest.py

--- quantopian_backtest.py
+++ quantopian_backtest.py
@@ -11,18 +11,26 @@ def preview(df):
     return df
 
 def initialize(context):
-    url = 'https://raw.githubusercontent.com/xlinGithub/Quantitative-Country-ETF-Trading-Strategy/9be718b4125d7f33e691b91693486884cfe91f0f/backtest_inputs.csv'
-    fetch_csv(url, 
-              date_column='Date',
-              pre_func = preview,
-              date_format='%Y-%m-%d')
+    url = 'https://raw.githubusercontent.com/xlinGithub/Quantitative-Country-ETF-Trading-Strategy/9be718b4125d7f33e691b91693486884cfe91f0f/backtest_inputs.csv' # long-only
+    url = 'https://raw.githubusercontent.com/xlinGithub/Quantitative-Country-ETF-Trading-Strategy/1da81303de9cc9b4fd315c7ab90b753831406d5e/backtest_inputs.csv' # long-short
+    url = 'https://raw.githubusercontent.com/xlinGithub/Quantitative-Country-ETF-Trading-Strategy/3317eae891e7ab96af8399f7c8709dbf4c905b77/backtest_inputs.csv' # long-only ex US
+    try:
+        fetch_csv(url, 
+                  date_column='Date',
+                  pre_func = preview,
+                  date_format='%Y/%m/%d')
+    except:
+        fetch_csv(url, 
+                  date_column='Date',
+                  pre_func = preview,
+                  date_format='%Y-%m-%d')
     
     context.rebalance_dates = ['2019-01-02', '2019-02-01', '2019-03-01', 
                                '2019-04-01', '2019-05-01', '2019-06-03', 
                                '2019-07-01', '2019-08-01', '2019-09-03',
                                '2019-10-01', '2019-11-01', '2019-12-02']
     
-    set_benchmark(symbol('ACWI'))
+    set_benchmark(symbol('ACWX'))
     set_slippage(slippage.FixedBasisPointsSlippage(basis_points=0.1, volume_limit=1))
     schedule_function(market_rebalance_process, date_rules.every_day(), time_rules.market_close(minutes=180))
     
